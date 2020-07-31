4b47f646fa456edff8e41290d00ee56e71ac3218
Vijay Kumar <vijay.kumar@agribazaar.com>
2020-07-31 08:17:56 +0530

    new functions

--- bin/index.js
+++ bin/index.js
@@ -2,35 +2,42 @@
 * obj: sample Object
 * returnType: HTML or CONSOLE
 */
-const getObjectString = (obj, returnType = 'HTML') => {
+const getObjectStringHtml = (obj) => {
     let keys = Object.keys(obj);
     let stringToReturn = "";
     keys.map(key => {
-
-        if (returnType === 'HTML') {
-            let keyToInt = parseInt(key, 10);
-            if(!isNaN(keyToInt)){
-                stringToReturn += `<p>====${key}====<p>${getObjectString(obj[key], returnType)}</p></p>`;
-            }else if(typeof obj[key] === 'object'){
-                stringToReturn += `<p> <u>${key.toUpperCase()}</u> <p>${getObjectString(obj[key], returnType)}</p></p>`;
-            }else{
-                stringToReturn += `<p> ${key} = ${obj[key]}</p>`;
-            }
-        } else if (returnType === 'CONSOLE') {
-            let keyToInt = parseInt(key, 10);
-            if(!isNaN(keyToInt)){
-                stringToReturn += `\n====${key}====\n${getObjectString(obj[key], returnType)}`;
-            }else if(typeof obj[key] === 'object'){
-                stringToReturn += `\n${key.toUpperCase()}  \n${getObjectString(obj[key], returnType)}\n`;
-            }else{
-                stringToReturn += `${key} = ${obj[key]}\n`;
-            }
-
+        let keyToInt = parseInt(key, 10);
+        if (!isNaN(keyToInt)) {
+            stringToReturn += `<p>====${key}====<p>${getObjectString(obj[key], returnType)}</p></p>`;
+        } else if (typeof obj[key] === 'object') {
+            stringToReturn += `<p> <u>${key.toUpperCase()}</u> <p>${getObjectString(obj[key], returnType)}</p></p>`;
+        } else {
+            stringToReturn += `<p> ${key} = ${obj[key]}</p>`;
         }
     });
     return stringToReturn;
 };
 
+
+const getObjectStringConsole = (obj) => {
+    let keys = Object.keys(obj);
+    let stringToReturn = "";
+    keys.map(key => {
+        let keyToInt = parseInt(key, 10);
+        if (!isNaN(keyToInt)) {
+            stringToReturn += `\n====${key}====\n${getObjectString(obj[key], returnType)}`;
+        } else if (typeof obj[key] === 'object') {
+            stringToReturn += `\n${key.toUpperCase()}  \n${getObjectString(obj[key], returnType)}\n`;
+        } else {
+            stringToReturn += `${key} = ${obj[key]}\n`;
+        }
+
+
+    });
+    return stringToReturn;
+};
+
 module.exports = {
-    getObjectString
+    getObjectStringHtml,
+    getObjectStringConsole
 };
